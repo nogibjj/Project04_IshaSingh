b77da5763425eb49f668d0d80f0e17308bef15e6
IshaSingh01 <112577810+IshaSingh01@users.noreply.github.com>
2022-12-16 18:12:00 -0800

    Add files via upload

---
+++ project04.py
@@ -0,0 +1,35 @@
+
+import joblib
+import pandas as pd
+
+
+
+#open pickle model
+
+nationality_naive_bayes = open("/Users/isingh/Desktop/naive_bayes.pkl","rb")
+nationality_cv = joblib.load(nationality_naive_bayes)
+
+
+# import FastAPI
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+
+# import uvicorn
+import uvicorn
+
+
+@app.get('/')
+async def index():
+  return {"text":"Our First route"}
+
+if __name__ == '__main__':
+    uvicorn.run(app,host="127.0.0.0",port=8000)
+
+
+
+
+
